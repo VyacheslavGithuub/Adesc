fa24d75efcc7bf5b7015c8d7e25eb4e6c3ade5bd
Vyacheslav Yeldyshov <slavae222@gmail.com>
2022-12-06 15:27:58 +0500

    add_tariff_cards

---
+++ src/script.js
@@ -0,0 +1,179 @@
+let tariff = document.querySelector(".tariff_block");
+
+let tariff_cards = [
+  {
+    title: "Тариф «Первый»",
+    description: "Полный пакет опций для максимального результата",
+    price: "2 499 ₽",
+    whenPaidPerYear: "в месяц при оплате за год",
+    people: [
+      {icon: "user", title: "10+ пользователей"}, 
+      {icon: "organization", title: "5+ юридических лиц"}, 
+    ],
+    services:[
+      {isState: true, title: "Неограниченные счета"}, 
+      {isState: true, title: "Учет по проектам"}, 
+      {isState: true, title: "Отчет о движении денег"}, 
+      {isState: true, title: "Отчет о прибылях и убытках"}, 
+      {isState: true, title: "Настройка прав доступа"}, 
+      {isState: true, title: "Бюджеты и план-факт анализ"}, 
+      {isState: true, title: "Согласование платежей"}, 
+      {isState: true, title: "Кредиты"}, 
+      {isState: true, title: "Основные средства"}, 
+      {isState: true, title: "Расширенная аналитика"}, 
+      {isState: true, title: "Прикрепление файлов"}, 
+      {isState: true, title: "Приоритетная поддержка"}
+    ],
+    button: "Протестировать"
+  },
+  {
+    title: "Тариф «Бизнес»",
+    description: "Инструмент для улучшения экономики бизнеса",
+    price: "2 082 ₽",
+    whenPaidPerYear: "в месяц при оплате за год",
+    people: [
+      {icon: "user", title: "10+ пользователей"}, 
+      {icon: "organization", title: "5+ юридических лиц"}, 
+    ],
+    services:[
+      {isState: true, title: "Неограниченные счета"}, 
+      {isState: true, title: "Учет по проектам"}, 
+      {isState: true, title: "Отчет о движении денег"}, 
+      {isState: true, title: "Отчет о прибылях и убытках"}, 
+      {isState: true, title: "Настройка прав доступа"}, 
+      {isState: true, title: "Бюджеты и план-факт анализ"}, 
+      {isState: true, title: "Согласование платежей"}, 
+      {isState: false, title: "Кредиты"}, 
+      {isState: false, title: "Основные средства"}, 
+      {isState: false, title: "Расширенная аналитика"}, 
+      {isState: false, title: "Прикрепление файлов"}, 
+      {isState: false , title: "Приоритетная поддержка"}
+    ],
+    button: "Протестировать"
+  },
+  {
+    title: "Тариф «Комфорт»",
+    description: "Подходит для стартапов и микробизнеса",
+    price: "1 749 ₽",
+    whenPaidPerYear: "в месяц при оплате за год",
+    people: [
+      {icon: "user", title: "10+ пользователей"}, 
+      {icon: "organization", title: "5+ юридических лиц"}, 
+    ],
+    services:[
+      {isState: true, title: "Неограниченные счета"}, 
+      {isState: true, title: "Учет по проектам"}, 
+      {isState: true, title: "Отчет о движении денег"}, 
+      {isState: true, title: "Отчет о прибылях и убытках"}, 
+      {isState: false, title: "Настройка прав доступа"}, 
+      {isState: false, title: "Бюджеты и план-факт анализ"}, 
+      {isState: false, title: "Согласование платежей"}, 
+      {isState: false, title: "Кредиты"}, 
+      {isState: false, title: "Основные средства"}, 
+      {isState: false, title: "Расширенная аналитика"}, 
+      {isState: false, title: "Прикрепление файлов"}, 
+      {isState: false , title: "Приоритетная поддержка"}
+    ],
+    button: "Протестировать"
+  },
+  {
+    title: "Тариф «Эконом»",
+    description: "Подходит для фрилансеров и самозанятых",
+    price: "833 ₽", 
+    whenPaidPerYear: "в месяц при оплате за год",
+    people: [
+      {icon: "user", title: "10+ пользователей"}, 
+      {icon: "organization", title: "5+ юридических лиц"}, 
+    ],
+    services:[
+      {isState: true, title: "Неограниченные счета"}, 
+      {isState: true, title: "Учет по проектам"}, 
+      {isState: true, title: "Отчет о движении денег"}, 
+      {isState: false, title: "Отчет о прибылях и убытках"}, 
+      {isState: false, title: "Настройка прав доступа"}, 
+      {isState: false, title: "Бюджеты и план-факт анализ"}, 
+      {isState: false, title: "Согласование платежей"}, 
+      {isState: false, title: "Кредиты"},  
+      {isState: false, title: "Основные средства"}, 
+      {isState: false, title: "Расширенная аналитика"}, 
+      {isState: false, title: "Прикрепление файлов"}, 
+      {isState: false , title: "Приоритетная поддержка"}
+    ],
+    button: "Протестировать"
+  }
+];
+// Создание карточек тарифов
+for (const item of tariff_cards) {
+  let tariff_card = document.createElement("div")
+  tariff_card.classList.add("tariff_item")
+
+  // Title
+  let title = document.createElement("h3")
+  title.classList.add("item_title")
+  title.innerHTML = item.title
+  tariff_card.append(title);
+  // Description
+  let description = document.createElement("p")
+  description.classList.add("item_description")
+  description.innerHTML =  item.description
+  tariff_card.append(description);
+  // Price
+  let price = document.createElement("h2")
+  price.classList.add("item_prace")
+  price.innerHTML =  item.price
+  tariff_card.append(price);
+  // whenPaidPerYear
+  let whenPaidPerYear = document.createElement("p")
+  whenPaidPerYear.classList.add("item_prace_description")
+  whenPaidPerYear.innerHTML =  item.whenPaidPerYear
+  tariff_card.append(whenPaidPerYear);
+
+  // people
+  // Создаём контейнер списка
+  let peopleUl = document.createElement("ul")
+  peopleUl.classList.add("card_ul")
+  tariff_card.append(peopleUl)
+  
+  for (const elem of item.people) {
+  // Создаём элемент списка
+    let peopleLi = document.createElement("li")
+    peopleLi.classList.add("card_li")
+  // Добавляем картинку 
+    let icon = document.createElement("div")
+    icon.classList.add(elem.icon === "user" ? "card_people_user" : "card_people_organization")
+    peopleLi.append(icon);
+  // Добавляем описание
+    let title = document.createElement("p")
+    title.classList.add("item_active")
+    title.innerHTML =  elem.title
+    peopleLi.append(title);
+  // Добавляем элемент списка в карточку
+  peopleUl.append(peopleLi)
+  }
+
+  //  services
+  for (const elem of item.services) {
+  // Создаём элемент списка
+    let servicesLi = document.createElement("li")
+    servicesLi.classList.add("card_li")
+  // Добавляем картинку 
+    let icon = document.createElement("div")
+    icon.classList.add(elem.isState ?"services_img-active" : "services_img-not_active")
+    servicesLi.append(icon);
+  // Добавляем описание
+    let title = document.createElement("p")
+    title.classList.add(elem.isState ?"item_active" : "item_not_active")
+    title.innerHTML =  elem.title
+    servicesLi.append(title);
+
+  // Добавляем элемент списка в карточку
+    tariff_card.append(servicesLi)
+  }
+//Card button
+  let card_button = document.createElement("button")
+  card_button.classList.add("card_button")
+  card_button.innerHTML =  item.button
+  tariff_card.append(card_button);
+
+  tariff.append(tariff_card)
+}
